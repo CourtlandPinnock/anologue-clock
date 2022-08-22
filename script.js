3a63668315dba1edde919c00f2dfbd1da29365c7
Courtland Pinnock <courtlandpinnock@gmail.com>
2022-08-22 17:36:34 +0100

    Add code to make clock "animation" seem smoother mimicking a mechanical style movement
    Interval and steps for updating hand postions are now less, and more constant. This makes the movement of the
    clock appear smoother as the hands move less each time, but more frequently, like that of a mechanical
    movement watch.
    User will be able to switch between "mechanical" and "normal" modes in future.

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function movement() {
 
     
     
-    let timeRun = setInterval(() => {
-        const time = new Date();
+   /* let timeRun = setInterval(() => {
+        
        let secondDeg = time.getSeconds() * 6;
         secondHand.style.transform = `rotate(${secondDeg}deg)`;
         //console.log(secondDeg);
@@ -21,8 +21,33 @@ function movement() {
         let hourDeg = (time.getHours() - 12) * 30;
         //console.log(hourDeg);
         hourHand.style.transform = `rotate(${hourDeg}deg)`;
-    }, 1000);
+    }, 1000); */
     
+    let timeMech = setInterval( () => {
+        const timeMech = new Date();
+        let secondDegMech = timeMech.getSeconds();
+        miliSecondDegMech = timeMech.getMilliseconds();
+        let currentMiliSec = secondDegMech * 1000 + miliSecondDegMech;
+        //console.log(currentMiliSec)
+        secondHand.style.transform = `rotate(${currentMiliSec * 0.006}deg)`;
+
+        let minuteDegMech = timeMech.getMinutes();
+        let currentMin = minuteDegMech * 60 * 1000 + currentMiliSec;
+        minuteHand.style.transform = `rotate(${currentMin * 0.0001}deg)`
+        //console.log(currentMin);
+
+        
+    }, 1);
+    
+    let timeHoursMech = setInterval(() => {
+        const time = new Date();
+        let currentMin = time.getMinutes();
+        let hoursMech = time.getHours();
+        hoursMech = (hoursMech - 12) * 60;
+        hoursMech = (hoursMech + currentMin) * 0.5;
+        console.log(hoursMech / 0.5);
+        hourHand.style.transform = `rotate(${hoursMech}deg)`;
+    }, 5);
 
 }
 
